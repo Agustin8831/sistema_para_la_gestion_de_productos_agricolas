ec5bc85dcc5b0cb22f3e34bf216f71b82debf322
agustinp_21 <apagustin8831@gmail.com>
2023-09-07 13:42:11 -0700

    first commit

--- README.md
+++ README.md
@@ -68,3 +68,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# sistema_para_la_gestion_de_productos_agricolas
--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header className='header'>
+      <h1>Sistema de gestion de productos</h1>
+      <img src="http://pinosanquintin.com.mx/img/logo.png" alt=""></img>
+      <nav className='nav'>
+        <ul className='ul'>
+          <li><a href="#" target='_blank'>Inicio</a></li>
+          <li><a href="#" target='_blank'>Productos</a></li>
+          <li><a href="#" target='_blank'>Cordinacion</a></li>
+          <li><a href="#" target='_blank'>Sesion</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main className='main'>
+      <section className='sec-funciones-principales'>
+        <article className='articulo-funciones'>
+          <img src="https://www.agri.mx/wp-content/uploads/2022/09/1.png" alt=""></img>
+          <div className='articulo-textos'>
+            <p className='articulo-encabezado'>Administar costos</p>
+            <p>Aqui puedes puedes calcular el costo de los productos</p>
+            <button className='articulo-boton'>Entrar</button>
+          </div>
+        </article>
+        <article className='articulo-funciones'>
+          <img src="https://www.agri.mx/wp-content/uploads/2022/09/2.png" alt=""></img>
+          <div className='articulo-textos'>
+            <p className='articulo-encabezado'>Planes</p>
+            <p>Aqui puedes agregar un plan semanal, en el cual se pueda trabajar sobre ella</p>
+            <button className='articulo-boton'>Entrar</button>
+          </div>
+        </article>
+        <article className='articulo-funciones'>
+          <img src="https://www.agri.mx/wp-content/uploads/2022/09/3.png" alt=""></img>
+          <div className='articulo-textos'>
+            <p className='articulo-encabezado'>Coordinacion</p>
+            <p>Aqui puedes cordinar los pedidos, en donde se envian y en que fechas</p>
+            <button className='articulo-boton'>Entrar</button>
+          </div>
+        </article>
+        <article className='articulo-funciones'>
+          <img src="https://www.agri.mx/wp-content/uploads/2022/09/4.png" alt=""></img>
+          <div className='articulo-textos'>
+            <p className='articulo-encabezado'>Administar productos del almacen</p>
+            <p>Aqui puedes agregar o eliminar los productos existentes que se encuentran en el almace</p>
+            <button className='articulo-boton'>Entrar</button>
+          </div>
+        </article>
+      </section>
+    </main>
+      
+    </>
   );
 }
 
